244843ae2e843fca2e903ca7659621d4e0fe3518
dudesahn <dudesahn@gmail.com>
2021-03-07 22:36:58 -0500

    fix: remove extra interfaces

--- contracts/linkZap.sol
+++ contracts/linkZap.sol
@@ -3,11 +3,9 @@ pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 // These are the core Yearn libraries
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./interfaces/curve.sol";
